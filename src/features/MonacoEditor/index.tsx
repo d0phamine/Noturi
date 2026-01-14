258c6f31b962c702a6cfde5ee11dc9467361d08e
gasizov <gasizov@mtsbank.ru>
2026-01-14 12:25:32 +0300

    adding monaco editor theme functionality

--- src/features/MonacoEditor/index.tsx
+++ src/features/MonacoEditor/index.tsx
@@ -5,15 +5,32 @@ import { FC, useEffect, useRef } from "react"
 import { observer } from "mobx-react-lite"
 import { editor } from "monaco-editor"
 
+import githubDarkTheme from "@/themes/GitHub Dark.json"
+import githubLightTheme from "@/themes/GitHub Light.json"
+
 import { useStores } from "@/store"
 
+import { useColorMode } from "@/components/ui-chakra"
+
 export const MonacoEditor: FC = observer(() => {
-	const { FsStore, WorkspaceStore } = useStores()
+	const { WorkspaceStore } = useStores()
+	const { colorMode } = useColorMode()
 
 	const { fileTabs, activeFileTabId } = WorkspaceStore.WorkspaceStoreData
 	const activeFile = fileTabs.find((tab) => tab.id === activeFileTabId)
 
 	const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null)
+
+	const handleEditorWillMount = (monaco: typeof import("monaco-editor")) => {
+		monaco.editor.defineTheme(
+			"githubDark",
+			githubDarkTheme as editor.IStandaloneThemeData
+		)
+		monaco.editor.defineTheme(
+			"githubLight",
+			githubLightTheme as editor.IStandaloneThemeData
+		)
+	}
 
 	useEffect(() => {
 		editorRef.current?.focus()
@@ -25,9 +42,10 @@ export const MonacoEditor: FC = observer(() => {
 				<Editor
 					key={activeFile?.id}
 					height="100%"
-					theme="vs-dark"
+					theme={colorMode === "dark" ? "githubDark" : "githubLight"}
 					value={activeFile?.content}
 					path={activeFile?.path}
+					beforeMount={handleEditorWillMount}
 					onMount={(editor) => (editorRef.current = editor)}
 					onChange={(newContent) =>
 						WorkspaceStore.setFileChanged(newContent ?? "")
